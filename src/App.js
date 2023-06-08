71b680e186a0f0bc295717ad51e023f4e1ca6737
lfcbivek <lfcbivek@gmail.com>
2023-06-07 20:36:56 -0400

    Style changes

--- src/App.js
+++ src/App.js
@@ -2,17 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/navbar/navbar';
 import GetData from './components/experience/experience';
+import Socials from './components/socials/socials';
 import React from 'react';
 function App() {
   GetData()
   return (
-    <React.Fragment>
-      <NavBar />
-      
-      <div className='mx-24'> 
-        <GetData />
-      </div>
-    </React.Fragment>
+    <div className='App'>
+      <React.Fragment>
+          <NavBar />
+          <div className = "content-container">
+            <div className='socials'>
+              <Socials />
+            </div>
+            <div className='content mx-24'>
+              <div className='mx-24'> 
+                <GetData />
+              </div>
+            </div>
+            
+          </div>
+      </React.Fragment>
+    </div>
   );
 }
 
--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -1,45 +1,46 @@
-import React, {useState} from 'react';
-import {db} from '../../data/firebase';
-import { doc, getDocs, collection } from 'firebase/firestore';
+import React, {useEffect, useState} from 'react';
+import {Experience} from '../../data/portfolio';
 import './experience.css'
 
 
 function GetData() {
-    const [entries,setEntry] = useState([]);
-    const dbRef = collection(db,"experience");
-    useState(() => {
-        const getData = async() => {
-            const data = await getDocs(dbRef);
-            data.forEach( d => console.log(d.data()))
-            setEntry(data)
-            console.log(JSON.stringify(data,null,4))
-            console.log("set entry",JSON.stringify(setEntry,null,4))
-        }
-        console.log(getData())
-    }, [])
 
+    const [selectedIndex, setSelectedIndex] = useState(0);
     const onCompanyHover = (e) => {
         console.log("hover")
-        e.target.style.background = 'red';
+        e.target.style.background = 'grey';
     }
 
     const onMouseOut = e => {
-        e.target.style.background = 'white';
+        e.target.style.background = '#61dafb';
     }
     return(
-        // <div className='App'>{entries.map((entry) => {
-        //     return(
-        //         <{entry}>
-        //     )
-        // })}</div>
-
         <div className='experience-component'>
             <div className = 'title'>
-                <h1 className='clear-sans-bold-italic'> 01 - Professional Experience</h1>
+                <h1 className='clear-sans-bold-italic'> Professional Experience</h1>
             </div>
-            <div className = "company-list">
-                <div className= "vertical-line"></div>
-                <div className = "companies" onMouseOver={onCompanyHover} onMouseOut={onMouseOut}>ABC</div>
+            <div className='description-component'>
+                <div className='experience'>
+                    {Experience && Experience.map(company => {
+                        return (
+                            <span onMouseOver={onCompanyHover} onMouseOut={onMouseOut}>
+                            {/* <span> */}
+                                <div className = "company-list">
+                                    <div className= "vertical-line"></div>
+                                    <div className = "companies">{company["Company"]}</div>
+                                </div>
+                            </span>
+                        );
+                        })
+                    }
+                </div>
+                <div className='job-description'>
+                    <h2>{Experience[selectedIndex]['Company']}</h2>
+                    <br />
+                    <ul>
+                        {Experience[selectedIndex]['Description'].map(data => (<div><li> {data}</li><br/></div>))}    
+                    </ul>
+                </div>
             </div>
         </div>
     )
